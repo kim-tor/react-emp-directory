3b140e462d33b6871d37fdb6c2148adf8a35d40d
Kim Torres <kimberley.5118@gmail.com>
2021-01-22 19:25:14 -0500

    added functionality to my employeecontainer.js, created multiple components to give structure to my render, added some styling

---
+++ src/components/card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Card(props) {
+ 
+    return (
+        <tr>
+          <th>
+            <img alt={props.firstName} src={props.picture} />
+          </th>
+          <td>{props.firstName}</td>
+          <td>{props.lastName}</td>
+          <td><a href={"mailto:" + props.email} target="__blank">
+            {props.email}
+          </a>
+          </td>
+          <td>{props.phone}</td>
+        </tr>
+      );
+    }
+export default Card;
---
+++ src/components/employeeContainer.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import Container from "./container";
+import Col from "./col";
+import Card from "./card";
+import Wrapper from "./wrapper";
+import SearchForm from "./searchForm";
+
+import API from "../utils/API";
+
+class EmployeeContainer extends Component {
+    state = {
+        result: [],
+        search: ""
+    };
+
+    // When this component mounts, search for specific employee
+    componentDidMount() {
+        this.firstEmployee()
+    }
+
+    firstEmployee = () => {
+        API.getUsers()
+            .then(res =>
+                this.setState({ result: res.data.results })
+            )
+            .catch(err => console.log(err));
+    };
+
+    searchEmployee = (query) => {
+        console.log(query)
+        API.getUsers(query)
+            .then(res => this.setState({ result: res.data.results }))
+            .catch(err => console.log(err));
+    };
+
+    handleInputChange = event => {
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    // When the form is submitted, search the Employee API for the value of `this.state.search`
+    handleFormSubmit = event => {
+        event.preventDefault();
+        const filteredResults = this.state.result.filter(employee => employee.name.first.includes(this.state.search))
+        console.log(this.state.search);
+        this.setState({ result: filteredResults });
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Container>
+                    <div className="header">
+                        <Col>
+                            <h1>Employee Directory</h1>
+                            <SearchForm
+                                value={this.state.search}
+                                handleInputChange={this.handleInputChange}
+                                handleFormSubmit={this.handleFormSubmit}
+                            />
+                        </Col>
+                    </div>
+
+                    <Col>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Photo</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {[...this.state.result].map((item) => (
+                                    <Card
+                                        picture={item.picture.large}
+                                        firstName={item.name.first}
+                                        lastName={item.name.last}
+                                        email={item.email}
+                                        phone={item.phone}
+                                        key={item.key}
+                                    />
+                                ))}
+                            </tbody>
+                        </table>
+                    </Col>
+
+                </Container>
+            </Wrapper>
+        )
+    }
+}
+export default EmployeeContainer;
